7aae7e5e61e0361be1178cf488fa7692c48ed77a
vladimir <vlj2007@yandex.ru>
2024-09-08 22:41:39 +0700

    I added the dependency version 2.16.2 I added class record Todo. I added text in method main.

--- src/main/java/my/homeproject/Application.java
+++ src/main/java/my/homeproject/Application.java
@@ -1,7 +1,17 @@
 package my.homeproject;
 
+
+import my.homeproject.todo.Todo;
+import my.homeproject.todo.TodoClient;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        TodoClient todoClient = new TodoClient();
+        List<Todo> todos = todoClient.findAll();
+        System.out.println(todos);
     }
 }
--- src/main/java/my/homeproject/todo/TodoClient.java
+++ src/main/java/my/homeproject/todo/TodoClient.java
@@ -1,10 +1,14 @@
 package my.homeproject.todo;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 /***
  * Класс для работы клиента
@@ -14,6 +18,7 @@ public class TodoClient {
 
     private final String BASE_URL = "https://jsonplaceholder.typicode.com/todos";
     private final HttpClient client;
+    private final ObjectMapper objectMapper;
 
     /***
      * Клиент для вызова других сервисов
@@ -21,22 +26,23 @@ public class TodoClient {
 
     public TodoClient() {
         client = HttpClient.newHttpClient();
+        objectMapper = new ObjectMapper();
     }
 
     /***
      * Метод для списка всех задач в системе, возвращает строку данных
      */
 
-    public String findAll() throws IOException, InterruptedException {
+    public List<Todo> findAll() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(BASE_URL))
                 .GET()
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        return response.body();
+        return objectMapper.readValue(response.body(), new TypeReference<>() {}) ;
     }
+
 
 
 }
