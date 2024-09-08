7b10caa70b6a30b0fc5196c84cd80e90602b62ce
vladimir <vlj2007@yandex.ru>
2024-09-08 16:25:45 +0700

    Commet init

---
+++ src/main/java/my/homeproject/Application.java
@@ -0,0 +1,7 @@
+package my.homeproject;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/my/homeproject/todo/TodoClient.java
@@ -0,0 +1,9 @@
+package my.homeproject.todo;
+
+public class TodoClient {
+    private final String BASE_URL = "https://jsonplaceholder.typicode.com/todos";
+
+    public TodoClient() {
+
+    }
+}
